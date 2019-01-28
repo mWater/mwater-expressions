9f7bae276bd5c3dbee40200609d9d83e41037a90
Clayton Grassick <clayton@claytronics.org>
2019-01-28 15:35:26 -0500

    Added expr cleaner td

--- index.d.ts
+++ index.d.ts
@@ -314,6 +314,23 @@ export class ExprCompiler {
   compileTable(table: string, alias: string): JsonQLFrom
 }
 
+/** Cleans expressions. Cleaning means nulling invalid (not just incomplete) expressions if they cannot be auto-fixed. */
+export class ExprCleaner {
+  constructor(schema: Schema, variables?: Variable[])
+
+  /** Clean an expression, returning null if completely invalid, otherwise removing
+    invalid parts. Attempts to correct invalid types by wrapping in other expressions.
+    e.g. if an enum is chosen when a boolean is required, it will be wrapped in "= any" op
+    options: 
+      table: optional current table. expression must be related to this table or will be stripped
+      types: optional types to limit to
+      enumValueIds: ids of enum values that are valid if type is enum
+      idTable: table that type of id must be from
+      aggrStatuses: statuses of aggregation to allow. list of "individual", "literal", "aggregate". Default: ["individual", "literal"]
+  */
+  cleanExpr(expr:Expr, options: { table?: string, types?: string[], enumValueIds?: string[], idTable?: string, aggrStatuses?: AggrStatus[] }): Expr
+}
+
 /** a row is a plain object that has the following functions as properties */
 export interface ExprEvaluatorRow {
   /** gets primary key of row. callback is called with (error, value) */
